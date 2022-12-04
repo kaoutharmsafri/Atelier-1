7e61e52bc891bb2009331e3b72a293e106d93abf
kaouthar m'safri <117312800+kaoutharmsafri@users.noreply.github.com>
2022-12-04 02:40:51 +0100

    Delete Atelier1_ex2.py

--- Atelier1_ex2.py
+++
@@ -1,8 +0,0 @@
-#Exercice 2 : Ecrire une fonction python qui calcul la factorielle d’un nombre donné
-a=int(input("Entrer un nombre: "))
-def factorielle (a):
-    j=1
-    for i in range (1,a+1):
-        j=i*j
-    return j           
-print('le factoriel du nombre %d est: '%a,factorielle(a))
