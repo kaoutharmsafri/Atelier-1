ae30bae30959baba1037b7c34284dcde6d650039
kaouthar m'safri <117312800+kaoutharmsafri@users.noreply.github.com>
2022-12-05 22:03:29 +0100

    Update Atelier1_ex4.py

--- Atelier1_ex4.py
+++ Atelier1_ex4.py
@@ -2,11 +2,14 @@
 List=[]
 x=int(input("Entrer un nombre: "))
 a=x
-while(x>0):
-        i=0
-        List.insert(i,x%2)
-        x=x//2
-        i+=1
+def binaire (x):
+        while(x>0):
+                i=0
+                List.insert(i,x%2)
+                x=x//2
+                i+=1
+        for i in range (len(List)):     
+                print(List[i],end="")
+        return List
 print('le nombre binaire du nombre décimal %d est : '%a,end=" ")
-for i in range (len(List)):     
-    print(List[i],end="")
+binaire (x)
